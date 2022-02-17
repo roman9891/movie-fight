af9be94b16421586ec29da42a8891c3e6a67ce8a
Roman Opalacz <roman9891@gmail.com>
2022-02-17 16:34:26 -0500

    added dropdown

--- index.js
+++ index.js
@@ -6,11 +6,44 @@ const fetchData = async searchTerm => {
         }
     })
 
-    console.log(response.data)
+    if (response.data.Error) return []
+
+    return response.data.Search
+}
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `
+    <label><b>Search for Movie</b></label>
+    <input class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`
+const input = document.querySelector('input')
+const dropdown = document.querySelector('.dropdown')
+const resultsWrapper = document.querySelector('.results')
+
+const onInput = async e => {
+    const movies = await fetchData(e.target.value)
+
+    dropdown.classList.add('is-active')
+    for (let movie of movies) {
+        const option = document.createElement('a')
+        const poster = movie.Poster === 'N/A' ? '' : movie.Poster
+
+        option.classList.add('dropdown-item')
+        option.innerHTML = `
+            <img src="${poster}"/>
+            ${movie.Title}
+        `
+
+        resultsWrapper.appendChild(option)
+    }
 }
 
-const input = document.querySelector('input')
-
-const onInput = e => fetchData(e.target.value)
-
 input.addEventListener('input', debounce(onInput, 500))
+
+document.addEventListener('click', e => {
+    if (!root.contains(e.target)) dropdown.classList.remove('is-active')
+})
