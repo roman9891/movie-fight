64935fc177fc14d9140c5630fef41904d8e90fc0
Roman Opalacz <roman9891@gmail.com>
2022-02-17 09:49:56 -0500

    added input and debounce

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hi there!');
+const fetchData = async searchTerm => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '54822106',
+            s: searchTerm,
+        }
+    })
+
+    console.log(response.data)
+}
+
+const input = document.querySelector('input')
+
+const debounce = (func, delay) => {
+    let timeoutID
+
+    return (...args) => {
+
+        if (timeoutID) clearTimeout(timeoutID)
+        
+        timeoutID = setTimeout(() => {
+            func.apply(null, args)
+        }, delay)
+    }
+}
+
+
+const onInput = e => fetchData(e.target.value)
+
+input.addEventListener('input', debounce(onInput, 500))
